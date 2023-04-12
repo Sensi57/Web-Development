cabec2dfe9a77d42afc784b2aa07f600874b45fc
Sensei57 <115139279+Sensei57@users.noreply.github.com>
2023-04-12 21:52:14 +0600

    Delete hh_back directory

--- hh_back/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Company
-from .models import Vacancy
-
-admin.site.register(Company)
-admin.site.register(Vacancy)
-
-# Register your models here.
